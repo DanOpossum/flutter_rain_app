335b7841925e654c6d1608853193b2bc1c8fb49b
djobrie <djobrie@g.clemson.edu>
2020-11-23 22:03:19 -0500

    Added grid with clickable images to select sounds

--- lib/MusicPlayer/MusicPlayerScreen.dart
+++ lib/MusicPlayer/MusicPlayerScreen.dart
@@ -11,13 +11,12 @@ class MusicPlayerScreen extends StatefulWidget {
 
 // The main UI for the Music Player
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
-
-  _MusicPlayerScreenState(){
-            // AudioService.start(
-            // backgroundTaskEntrypoint: _audioTaskEntryPoint,
-            // androidNotificationChannelName: 'Audio Service Demo',
-            // androidNotificationColor: 0xFF2222f5,
-            // androidNotificationIcon: 'mipmap/ic_launcher');
+  _MusicPlayerScreenState() {
+    // AudioService.start(
+    // backgroundTaskEntrypoint: _audioTaskEntryPoint,
+    // androidNotificationChannelName: 'Audio Service Demo',
+    // androidNotificationColor: 0xFF2222f5,
+    // androidNotificationIcon: 'mipmap/ic_launcher');
   }
 
   @override
@@ -26,81 +25,72 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
       appBar: AppBar(
         title: Text('Music Player'),
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        color: Colors.white,
-        child: new Column(
-        children:[
-          new Image(image:AssetImage('assets/kitty-8-10.jpg'))
-          ,
-        // The background audio player will communicate with the help of streams to the ui
-        // we are listening to the audioplayers streams
-        new StreamBuilder<AudioState>(
-          stream: _audioStateStream,
-          builder: (context, snapshot) {
-            final audioState = snapshot.data;
-            final queue = audioState?.queue;
-            final mediaItem = audioState?.mediaItem;
-            final playBackState = audioState?.playbackState;
-            final processingState =
-                playBackState?.processingState ?? AudioProcessingState.none;
-            final playing = playBackState?.playing ?? false;
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          color: Colors.white,
+          child: new Column(mainAxisSize: MainAxisSize.max, children: [
+            // The background audio player will communicate with the help of streams to the ui
+            // we are listening to the audioplayers streams
+            new StreamBuilder<AudioState>(
+              stream: _audioStateStream,
+              builder: (context, snapshot) {
+                final audioState = snapshot.data;
+                final queue = audioState?.queue;
+                final mediaItem = audioState?.mediaItem;
+                final playBackState = audioState?.playbackState;
+                final processingState =
+                    playBackState?.processingState ?? AudioProcessingState.none;
+                final playing = playBackState?.playing ?? false;
 
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    if (processingState == AudioProcessingState.none) ...[
-                      _startAudioPlayBtn(),
-
-                      //why wont this work??? tbd
-                      // _soundChoices(),
-                      
-                    ] else ...[
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        // The main media player buttons
-                        children: [
-                          !playing
-                              ? IconButton(
-                                  icon: Icon(Icons.play_arrow),
-                                  iconSize: 64.0,
-                                  onPressed: AudioService.play)
-                              : IconButton(
-                                  icon: Icon(Icons.pause),
-                                  iconSize: 64.0,
-                                  onPressed: AudioService.pause),
-                          IconButton(
-                              icon: Icon(Icons.skip_previous),
-                              iconSize: 64.0,
-                              onPressed: () {
-                                if (mediaItem == queue.first) {
-                                  return;
-                                }
-                                AudioService.skipToPrevious();
-                              }),
-                          IconButton(
-                              icon: Icon(Icons.skip_next),
-                              iconSize: 64.0,
-                              onPressed: () {
-                                if (mediaItem == queue.last) {
-                                  return;
-                                }
-                                AudioService.skipToNext;
-                              }),
-                        ],
-                      )
-                    ]
-                  ],
-                ));
-          },
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        _soundChoices(),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          // The main media player buttons
+                          children: [
+                            !playing
+                                ? IconButton(
+                                    icon: Icon(Icons.play_arrow),
+                                    iconSize: 64.0,
+                                    onPressed: AudioService.play)
+                                : IconButton(
+                                    icon: Icon(Icons.pause),
+                                    iconSize: 64.0,
+                                    onPressed: AudioService.pause),
+                            IconButton(
+                                icon: Icon(Icons.skip_previous),
+                                iconSize: 64.0,
+                                onPressed: () {
+                                  if (mediaItem == queue.first) {
+                                    return;
+                                  }
+                                  AudioService.skipToPrevious();
+                                }),
+                            IconButton(
+                                icon: Icon(Icons.skip_next),
+                                iconSize: 64.0,
+                                onPressed: () {
+                                  if (mediaItem == queue.last) {
+                                    return;
+                                  }
+                                  AudioService.skipToNext;
+                                }),
+                          ],
+                        )
+                      ],
+                    ));
+              },
+            ),
+          ]),
         ),
-        ]
-      ),
       ),
     );
   }
@@ -110,27 +100,75 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     return MaterialButton(
       child: Text('Start Audio Player'),
       onPressed: () async {
-         AudioService.start(
+        AudioService.start(
             backgroundTaskEntrypoint: _audioTaskEntryPoint,
             androidNotificationChannelName: 'Audio Service Demo',
             androidNotificationColor: 0xFF2222f5,
             androidNotificationIcon: 'mipmap/ic_launcher');
-        },
-      );
-      }
+      },
+    );
+  }
 
-  // _soundChoices() {
-  //   return GridView.count(
-  //     crossAxisCount: 2 ,
-  //     children: List.generate(50,(index){
-  //       return Container(
-  //         child: Card(
-  //         color: Colors.blue,
-  //       ),
-  //     );
-  //   }),
-  //   );
-  // }
+  // Can use this to check if playing
+  // if (processingState == AudioProcessingState.none) ...[
+
+  _soundChoices() {
+    return Container(
+      child: CustomScrollView(
+        shrinkWrap: true,
+        slivers: <Widget>[
+          SliverGrid(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            delegate: SliverChildListDelegate(
+              [
+                IconButton(
+                  icon: Image.asset('assets/kitty-8-10.jpg'),
+                  iconSize: 50,
+                  onPressed: () async {
+                    AudioService.start(
+                        backgroundTaskEntrypoint: _audioTaskEntryPoint,
+                        androidNotificationChannelName: 'Audio Service Demo',
+                        androidNotificationColor: 0xFF2222f5,
+                        androidNotificationIcon: 'mipmap/ic_launcher');
+                  },
+                ),
+                IconButton(
+                    icon: Image.asset('assets/kitty-8-10.jpg'),
+                    iconSize: 50,
+                    onPressed: () {}),
+                IconButton(
+                    icon: Image.asset('assets/kitty-8-10.jpg'),
+                    iconSize: 50,
+                    onPressed: () {}),
+                IconButton(
+                    icon: Image.asset('assets/kitty-8-10.jpg'),
+                    iconSize: 50,
+                    onPressed: () {}),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+
+
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  final Color color;
+
+  BodyWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10.0,
+      color: color,
+      alignment: Alignment.center,
+    );
+  }
 }
 
 void _audioTaskEntryPoint() async {
